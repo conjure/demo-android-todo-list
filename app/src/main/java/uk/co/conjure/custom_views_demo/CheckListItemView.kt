2a8bf85583f5151a6dd3f219a51973ae0914e4f5
Samuel Harvey-Collins <samuel.amco@gmail.com>
2021-09-27 11:35:35 +0100

    Animated strike through check list items

---
+++ app/src/main/java/uk/co/conjure/custom_views_demo/CheckListItemView.kt
@@ -0,0 +1,85 @@
+package uk.co.conjure.custom_views_demo
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewPropertyAnimator
+import android.view.animation.ScaleAnimation
+import android.widget.Checkable
+import android.widget.CompoundButton
+import android.widget.FrameLayout
+import android.widget.TextView
+
+class CheckListItemView : FrameLayout, Checkable {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    private val name: TextView
+    private val strikethrough: View
+
+    var text: String
+        get() {
+            return name.text.toString()
+        }
+        set(value) {
+            name.text = value
+        }
+
+    private var isChecked = false
+    private var animation: ViewPropertyAnimator? = null
+    private var checkedChangeListener: ((Boolean) -> Unit)? = null
+
+    init {
+        inflate(context, R.layout.item_todo, this)
+        name = findViewById(R.id.tv_item_name)
+        strikethrough = findViewById(R.id.strike_through)
+
+        strikethrough.visibility = View.VISIBLE
+        strikethrough.pivotX = 0f
+        strikethrough.scaleX = 0f
+
+        setOnClickListener { toggle() }
+    }
+
+    fun setChecked(checked: Boolean, animate: Boolean) {
+        if (checked == isChecked) return
+        isChecked = checked
+
+        if (animate) {
+            if (isChecked) animateStrikeThrough(true)
+            else animateStrikeThrough(false)
+        } else {
+            strikethrough.scaleX = if (isChecked) 1f else 0f
+        }
+
+        checkedChangeListener?.invoke(isChecked)
+    }
+
+    override fun setChecked(checked: Boolean) {
+        setChecked(checked, true)
+    }
+
+    private fun animateStrikeThrough(animateIn: Boolean) {
+        animation?.cancel()
+        strikethrough.scaleX = if (animateIn) 0f else 1f
+        animation = strikethrough.animate()
+            .scaleX(if (animateIn) 1f else 0f)
+            .setDuration(resources.getInteger(android.R.integer.config_mediumAnimTime).toLong())
+        animation?.start()
+    }
+
+    override fun isChecked() = isChecked
+
+    override fun toggle() {
+        setChecked(!isChecked)
+    }
+
+    fun setOnCheckedChangeListener(listener: ((Boolean) -> Unit)?) {
+        checkedChangeListener = listener
+    }
+}
--- app/src/main/java/uk/co/conjure/custom_views_demo/TodoItemAdapter.kt
+++ app/src/main/java/uk/co/conjure/custom_views_demo/TodoItemAdapter.kt
@@ -1,9 +1,8 @@
 package uk.co.conjure.custom_views_demo
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import uk.co.conjure.custom_views_demo.databinding.ItemTodoBinding
 
 class TodoItemAdapter(private val onCheckedChangeListener: OnItemCheckedListener) :
     RecyclerView.Adapter<TodoItemAdapter.TodoItemViewHolder>() {
@@ -19,23 +18,25 @@ class TodoItemAdapter(private val onCheckedChangeListener: OnItemCheckedListener
     }
 
     fun onNewList(items: List<TodoItem>) {
+        val diffCallback = ListDiffCallback(todoItems, items)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
         todoItems.clear()
         todoItems.addAll(items)
-        notifyDataSetChanged()
+        diffResult.dispatchUpdatesTo(this)
     }
 
     override fun getItemCount() = todoItems.size
 
     class TodoItemViewHolder(
-        private val binding: ItemTodoBinding,
+        private val checkListItemView: CheckListItemView,
         private val onCheckedChangeListener: OnItemCheckedListener
     ) :
-        RecyclerView.ViewHolder(binding.root) {
+        RecyclerView.ViewHolder(checkListItemView) {
         fun bind(todoItem: TodoItem) {
-            binding.root.text = todoItem.name
-            binding.root.setOnCheckedChangeListener { _, _ -> }
-            binding.root.isChecked = todoItem.done
-            binding.root.setOnCheckedChangeListener { _, isChecked ->
+            checkListItemView.text = todoItem.name
+            checkListItemView.setOnCheckedChangeListener(null)
+            checkListItemView.setChecked(todoItem.done, false)
+            checkListItemView.setOnCheckedChangeListener { isChecked ->
                 if (isChecked != todoItem.done)
                     onCheckedChangeListener.onCheckChanged(todoItem.id, isChecked)
             }
@@ -46,12 +47,28 @@ class TodoItemAdapter(private val onCheckedChangeListener: OnItemCheckedListener
                 parent: ViewGroup,
                 onCheckedChangeListener: OnItemCheckedListener
             ): TodoItemViewHolder {
-                val layoutInflater = LayoutInflater.from(parent.context)
-                val binding = ItemTodoBinding.inflate(layoutInflater, parent, false)
-                return TodoItemViewHolder(binding, onCheckedChangeListener)
+                val view = CheckListItemView(parent.context)
+                return TodoItemViewHolder(view, onCheckedChangeListener)
             }
         }
     }
 
     class OnItemCheckedListener(val onCheckChanged: (id: Int, checked: Boolean) -> Unit)
+
+    private class ListDiffCallback(
+        val oldList: List<TodoItem>,
+        val newList: List<TodoItem>
+    ) : DiffUtil.Callback() {
+        override fun getOldListSize(): Int = oldList.size
+
+        override fun getNewListSize(): Int = newList.size
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition].id == newList[newItemPosition].id
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition].id == newList[newItemPosition].id
+        }
+    }
 }
