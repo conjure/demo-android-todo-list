7789354de8aed2e2e95a181fe0e76ad377a2d56b
Samuel Harvey-Collins <samuel.amco@gmail.com>
2021-10-21 11:39:13 +0100

    Remove un-used todo items

--- app/src/main/java/uk/co/conjure/custom_views_demo/CheckListItemView.kt
+++ app/src/main/java/uk/co/conjure/custom_views_demo/CheckListItemView.kt
@@ -4,9 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewPropertyAnimator
-import android.view.animation.ScaleAnimation
 import android.widget.Checkable
-import android.widget.CompoundButton
 import android.widget.FrameLayout
 import android.widget.TextView
 
